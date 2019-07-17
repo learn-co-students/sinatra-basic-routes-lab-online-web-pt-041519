5d859adbcffcfeb47dc0c733e25131b31b268a07
lanibon <nguyenlan13@gmail.com>
2019-07-16 21:10:46 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Lan"
+    end
+
+    get '/hometown' do
+        "My hometown is Riverside"
+    end
+   
+    get '/favorite-song' do
+        "My favorite song is sun and moon"
+    end
+
+
 end
