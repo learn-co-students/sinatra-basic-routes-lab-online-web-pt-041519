5a254523ec5b02e687329fd082dc33d1b4142ae4
aparkening <aaron.parkening@gmail.com>
2019-07-17 15:58:01 -0400

    Done. Different routes run.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Pepperjack"
+  end
+
+  get '/hometown' do
+    "My hometown is Wondertown, PA"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Slice"
+  end
+
 end
