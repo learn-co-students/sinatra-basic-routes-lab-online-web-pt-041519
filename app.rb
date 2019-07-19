f4425823154a10eb90749b462e03c5e1be561e41
Craig Donavin <crdonavin@gmail.com>
2019-07-19 03:25:05 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,12 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Craig"
+  end
+
+  get('/hometown') {"My hometown is the Moon"}
+
+  get('/favorite-song'){"My favorite song is the screams of my enemies"}
+
 end
